7a3424d910ac0ebaf7e65445afea9199ef342b9d
connor <connoroakvillea32@hotmail.com>
2022-08-22 14:30:23 -0400

    deleted uneeded files, created component and container folders, created first component - card

---
+++ src/Containers/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Card from "../Components/Card.js"
+
+class App extends React.Component {
+
+    render() {
+
+        return(
+            <div>
+
+            </div>
+        );
+
+    }
+
+}
+
+export default App;
